f35adfe890d7b317155b05a185385b9a3143a463
furssov <fursovd70@gmail.com>
2022-06-26 22:19:02 +0300

    final

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,21 +5,24 @@ import ArrayApp.Arrays;
 
 public class Main {
     public static void main(String[] args)  {
-        Arrays arrays = new Arrays(4);
-        try {
 
-            arrays.addElement(3);
-            arrays.addElement(4);
-            arrays.addElement(4);
-            arrays.addElement(5);
+        Arrays arrays = new Arrays(100000);
 
-            arrays.deleteAllElements();
 
-            arrays.arrayOutput();
-        }
-        catch (ArrayException a)
-        {
-            System.out.println(a.getMessage());
-        }
+                arrays.fillRandom(200000);
+
+                 long before = System.currentTimeMillis();
+            arrays.bubleSort();
+            long after = System.currentTimeMillis();
+            System.out.println(after - before + " - 1");
+
+        before = System.currentTimeMillis();
+        arrays.insertionSort();
+        after = System.currentTimeMillis();
+        System.out.println(after - before + " - 2");
+
+
+
+
     }
 }
